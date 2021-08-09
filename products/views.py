c53bdda9de2decbd6cd1031a8b44ee5220a526f9
mchoud <mchoud@gmail.com>
2021-08-09 14:39:18 +0700

    tambah 404

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, JsonResponse
+from django.http.response import Http404
 from django.shortcuts import render
 
 from products.models import Product
@@ -8,7 +9,11 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello ggworld <h1>")
 
 def product_detail_view(request, id):
-    obj = Product.objects.get(id=id)
+    try:
+        obj = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        raise Http404
+        
     return HttpResponse(f"Product id {obj.id}")
 
 # def product_detail_view(request, *args, **kwargs):
