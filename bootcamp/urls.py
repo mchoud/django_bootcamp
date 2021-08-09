56f02c84863086019c8b27714d098817e2a83554
mchoud <mchoud@gmail.com>
2021-08-09 14:31:43 +0700

    berhasil

--- bootcamp/urls.py
+++ bootcamp/urls.py
@@ -6,6 +6,7 @@ from products import views
 
 urlpatterns = [
     path('search/', views.home_view),
+    path('products/<int:id>/', views.product_detail_view),
     #path('products/1/', views.product_detail_view),
     path('api/products/1/', views.product_api_detail_view),
     path('admin/', admin.site.urls),
--- products/views.py
+++ products/views.py
@@ -7,9 +7,14 @@ def home_view(request, *args, **kwargs):
 
     return HttpResponse("<h1>Hello ggworld <h1>")
 
-def product_detail_view(request, *args, **kwargs):
-    obj = Product.objects.get(id=1)
+def product_detail_view(request, id):
+    obj = Product.objects.get(id=id)
     return HttpResponse(f"Product id {obj.id}")
+
+# def product_detail_view(request, *args, **kwargs):
+#     obj = Product.objects.get(id=1)
+#     return HttpResponse(f"Product id {obj.id}")
+
 
 def product_api_detail_view(request, *args, **kwargs):
     obj = Product.objects.get(id=1)
