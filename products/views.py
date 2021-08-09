95c6e647c5fc1ea38d2378499a1b82c0bbefe15c
M. Cholid <mchoud@gmail.com>
2021-08-09 13:34:46 +0700

    jj

--- products/views.py
+++ products/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from products.models import Product
 # Create your views here.
 def home_view(request, *args, **kwargs):
 
     return HttpResponse("<h1>Hello ggworld <h1>")
+
+def product_detail_view(request, *args, **kwargs):
+    obj = Product.objects.get(id=1)
+    return HttpResponse(f"Product id {obj.id}")
 
 # class HomeView():
 #     pass
